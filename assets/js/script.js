21c2f53c5ed05edda5f49cbb2faea913cd898622
Jorge Garit <jorgegarit@gmail.com>
2022-07-15 00:30:49 -0400

    can not get my moment.js to appear on screen commiting for now whille tackle after

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,16 @@ var two = $("#2pm");
 var three = $("#3pm");
 var four = $("#4pm");
 var five = $("#5pm");
+
+// vars for moment.js current day and date
+var todaysDate = moment().format('ddd') + ", " + moment().format("MMM Do YY");
+
+
+const checkMoment= function(date) {
+    if (!date.isValid){
+        console.log("this is not a moment object");
+    } 
+    else {
+        console.log("this is a moment object");
+    }
+}
