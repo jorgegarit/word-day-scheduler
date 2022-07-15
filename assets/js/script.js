59625ffe69440270f0d5f7cc205a7503e5ee8286
Jorge Garit <jorgegarit@gmail.com>
2022-07-14 23:54:10 -0400

    added the vars on script.js for the hopurs of the work day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-window.alert("Im connected to html");
+// Vars for work day hours
+
+var nine = $("#9am");
+var ten = $("#10am");
+var eleven = $("#11am");
+var twelve = $("#12pm");
+var one = $("#1pm");
+var two = $("#2pm");
+var three = $("#3pm");
+var four = $("#4pm");
+var five = $("#5pm");
